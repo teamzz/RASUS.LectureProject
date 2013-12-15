2d3965c9c59a35fc6029a23d076f5b30bcfac5fe
filippm <filip.cajko@gmail.com>
2013-12-15 18:52:48 +0100

    Dodana funkcija za dobivanje kviza po imenu. Dodane metode hasch i equals u Statisticpicture

--- RASUSLab/src/main/java/hr/fer/ztel/domain/StatisticPicture.java
+++ RASUSLab/src/main/java/hr/fer/ztel/domain/StatisticPicture.java
@@ -1,6 +1,7 @@
 package hr.fer.ztel.domain;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -53,6 +54,41 @@ public class StatisticPicture implements Serializable {
 
 	public void setPicture(byte[] picture) {
 		this.picture = picture;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(picture);
+		result = prime * result
+				+ ((question == null) ? 0 : question.hashCode());
+		result = prime * result + ((quiz == null) ? 0 : quiz.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StatisticPicture other = (StatisticPicture) obj;
+		if (!Arrays.equals(picture, other.picture))
+			return false;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		if (quiz == null) {
+			if (other.quiz != null)
+				return false;
+		} else if (!quiz.equals(other.quiz))
+			return false;
+		return true;
 	}
 
 }
--- RASUSLab/src/main/java/hr/fer/ztel/service/QuizService.java
+++ RASUSLab/src/main/java/hr/fer/ztel/service/QuizService.java
@@ -1,24 +1,23 @@
 package hr.fer.ztel.service;
 
-import java.util.List;
-
 import hr.fer.ztel.dao.QuizDao;
-import hr.fer.ztel.domain.Question;
-
+import hr.fer.ztel.domain.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuizService {
-	
-	@Autowired
-	private QuizDao qiuzDao;
 
-	
-	
-	
-	
-	
-	
-	
+	@Autowired
+	private QuizDao quizDao;
+
+	public Quiz getQuizByCode(String code) {
+
+		for (Quiz tempQuiz : quizDao.list()) {
+			if (tempQuiz.getCode().equals(code))
+				return tempQuiz;
+		}
+		return null;
+	}
+
 }
